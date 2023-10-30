ded1f9096bddd1bcadff944a33941fad0780038f
Ahnseungc <94547692+Ahnseungc@users.noreply.github.com>
2023-10-30 14:12:34 +0900

    초기세팅 최종

---
+++ src/Components/Btn/index.jsx
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import React from 'react';
+
+const Btn = () => {
+  return <Button as="input" type="button" value="Input" />;
+};
+
+export default Btn;
--- src/Layouts/Main/index.jsx
+++ src/Layouts/Main/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Btn from 'Components/Btn';
 
 const Main = () => {
-  return <div>메인페이지</div>;
+  return (
+    <div>
+      메인페이지
+      <Btn />
+    </div>
+  );
 };
 
 export default Main;
